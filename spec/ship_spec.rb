0271fd52cb9078709f74aae2db58929fc3e6522e
Rob Schlereth <robert.schlereth@gmail.com>
2022-05-16 21:48:24 -0500

    feature: ship initialize instance of ship

--- spec/ship_spec.rb
+++ spec/ship_spec.rb
@@ -1,6 +1,6 @@
 require './lib/ship'
 
-Rspec.describe Ship do
+RSpec.describe Ship do
   before(:each) do
     @cruiser = Ship.new("Cruiser", 3)
   end
@@ -9,35 +9,35 @@ Rspec.describe Ship do
     expect(@cruiser).to be_instance_of(Ship)
   end
 
-  it "has a name" do
+  xit "has a name" do
     expect(cruiser.name).to equal("Cruiser")
   end
 
-  it "has a length" do
+  xit "has a length" do
     expect(cruiser.length).to equal(3)
   end
 
   # change name once code is created
-  it "has health" do
+  xit "has health" do
     expect(cruiser.health).to equal(3)
   end
 
-  it "indicates if it has been sunk" do
+  xit "indicates if it has been sunk" do
     expect(cruiser.sunk?).to equal(false)
   end
 
-  it "has been hit once" do
+  xit "has been hit once" do
     cruiser.hit
     expect(cruiser.health).to equal(2)
   end
 
-  it "has been hit twice" do
+  xit "has been hit twice" do
     2.times do cruiser.hit end
     expect(cruiser.health).to equal(1)
     expect(cruiser.sunk?).to equal(false)
   end
 
-  it "has been hit three times" do
+  xit "has been hit three times" do
     3.times do cruiser.hit end
     expect(cruiser.health).to equal(0)
     expect(cruiser.sunk?).to equal(true)
