427bbe9a8ac34de327fb88dd8955dd9697f36d34
Rob Schlereth <robert.schlereth@gmail.com>
2022-05-18 09:22:19 -0500

    test: add board cells

---
+++ spec/board_spec.rb
@@ -0,0 +1,19 @@
+require './lib/board'
+require './lib/cell'
+require 'pry'
+
+RSpec.describe Board do
+  before(:each) do
+    @board = Board.new
+  end
+
+  it "has a board" do
+    expect(@board).to be_an_instance_of(Board)
+  end
+
+  it "has cells" do
+    expect(@board.cells.class).to eq(Hash)
+    expect(@board.cells.count).to eq(16)
+    expect(@board.cells[A2]).to be_an_instance_of(Cell)
+  end
+end
