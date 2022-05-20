d6c7bcd995815b88490db5b0c2e4f94879d6a0ad
Mayu Takeda <mayu.takeda.utd@gmail.com>
2022-05-20 12:36:08 -0500

    feature: push partial place method for independent coding

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -63,9 +63,9 @@ RSpec.describe Board do
   it "can place ships" do
     cruiser = Ship.new("Cruiser", 3)
     @board.place(cruiser, ["A1", "A2", "A3"])
-    cell_1 = board.cells["A1"]
-    cell_2 = board.cells["A2"]
-    cell_3 = board.cells["A3"]
+    cell_1 = @board.cells["A1"]
+    cell_2 = @board.cells["A2"]
+    cell_3 = @board.cells["A3"]
 
     expect(cell_1).to be_instance_of(Cell)
     expect(cell_2).to be_instance_of(Cell)
