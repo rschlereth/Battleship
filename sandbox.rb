81c3e87e442763dce4eeb46506fe0577006f2da7
Mayu Takeda <mayu.takeda.utd@gmail.com>
2022-05-20 14:23:18 -0500

    feature: place ships

--- sandbox.rb
+++ sandbox.rb
@@ -1,31 +1,6 @@
 require 'pry'
 
-def valid_placement?( coordinates)
-  valid_pair = 0
-  # testing to see if the ship length is the same as the number of cells
-  if ship.length != coordinates.count
-    return false
-  else
-    coordinates.each_cons(2) do |cd1, cd2|
-      if compare_letters(cd1, cd2) != 0 || compare_letters(cd1, cd2) != -1
-        return false
-      elsif compare_letters(cd1, cd2) == -1 && compare_numbers(cd1, cd2) != 0
-        return false
-      elsif compare_letters(cd1, cd2) == 0 && compare_numbers != -1
-        return false
-      else
-        valid_pair += 1
-      end
-    end
-  end
-  true if valid_pair == coordinates.count - 1
-end
 
-cruiser = Ship.new("Cruiser", 3)
-submarine = Ship.new("Submarine", 2)
-
-p valid_placement?(submarine, ["A1", "A2"])
-p valid_placement?(cruiser, ["B1", "C1", "D1"])
 # def test_method
 #   test_array = [1, 2, 3]
 #   test_array.each_cons(2).all? do |num1, num2|
