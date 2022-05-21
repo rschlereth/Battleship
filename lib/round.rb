6928000fd8da39b40c7077cb68715efa88fa39b8
Rob Schlereth <robert.schlereth@gmail.com>
2022-05-21 12:33:01 -0500

    feature: start computer placement method

--- lib/round.rb
+++ lib/round.rb
@@ -5,6 +5,24 @@ class Round
     start_button = gets
     if start_button = "q"
       puts "Until next time!"
+    elsif start_button = "p"
+
     end
+  end
+
+  def computer_placement
+    coordinates = []
+    letters = ["A", "B", "C", "D"]
+    nums = ["1", "2", "3", "4"]
+    first_cell = letters.sample + nums.sample
+    coordinates << first_cell
+    
+    cell_index = 0
+    r1 = [0, 1, 2, 3].sample
+    until cell_index == @ship.length - 1
+      if r1 == 0 && coordinates[cell_index][1..-1].to_i > 1
+        num = coordinates[cell_index][1..-1].to_i -1
+        coordinates << coordinates[cell_index][0] + num.to_s
+
   end
 end
