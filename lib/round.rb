531d35f4425c2bd8459b96d4b349d808d541d687
Mayu Takeda <mayu.takeda.utd@gmail.com>
2022-05-21 20:03:04 -0500

    feature: wrote code that displays player and computer guess outcomes

--- lib/round.rb
+++ lib/round.rb
@@ -95,10 +95,50 @@ class Round
     # Computer guesses square
     # hash.keys => [array of keys].sample
     computer_guess = @board_player.cells.keys.sample
-    @board_player.cells.keys.delete(computer_guess)
+    @board_player.cells.keys - [computer_guess]
 
+    # Diplay Results
+    # To do:
+    # Shoot at cell for both player and computer
+    # board.cells["A1"] --> access to Cell A1 and all the Cell methods
+    # board.cells[key] => Cell.new("key")
+    @board_computer.cells[player_guess].fire_upon
+    @board_player.cells[computer_guess].fire_upon
+  # Tell player the outcome of both shots
+    # Your shot on A4 was a miss.
+    # My shot on C1 was a miss.
+    # player_guess = "A1"
+    # .render_cell => "H", "M", "X"
 
+    # Player guess outcome
+    if @board_computer.cells[player_guess].render_cell == "H"
+      puts "Your shot on #{player_guess} hit a ship."
+    elsif @board_computer.cells[player_guess].render_cell == "M"
+      puts "Your shot on #{player_guess} was a miss."
+    elsif @board_computer.cells[player_guess].render_cell == "X"
+      if @board_computer.cells[player_guess].ship.length == 2
+        puts "Your shot on #{player_guess} hit a ship. You sank my submarine!"
+      elsif @board_computer.cells[player_guess].ship.length == 3
+        puts "Your shot on #{player_guess} hit a ship. You sank my cruiser!"
+      else
+        puts "Your shot on #{player_guess} hit a ship. You sank my ship!"
+      end
+    end
 
+    # Computer guess outcome
+    if @board_player.cells[computer_guess].render_cell == "H"
+      puts "My shot on #{computer_guess} hit a ship."
+    elsif @board_player.cells[computer_guess].render_cell == "M"
+      puts "My shot on #{computer_guess} was a miss."
+    elsif @board_player.cells[computer_guess].render_cell == "X"
+      if @board_player.cells[computer_guess].ship.length == 2
+        puts "My shot on #{computer_guess} hit a ship. I sank your submarine!"
+      elsif @board_player.cells[computer_guess].ship.length == 3
+        puts "My shot on #{computer_guess} hit a ship. I sank your cruiser!"
+      else
+        puts "My shot on #{computer_guess} hit a ship. I sank your ship!"
+      end
+    end
   end
 
   def computer_placement(ship)
